1d134599ccaf65be975840e4b0e363cc3aa284a0
Nina <259290@pwr.edu.pl>
2021-03-03 23:35:00 +0100

    nwm

--- test3/modul.c
+++ test3/modul.c
@@ -1,4 +1,4 @@
-
+#include "modul.h"
 /*
  *  Zestaw wazniejszych stalych
  */
--- test3/program.c
+++ test3/program.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "modul.h"
 
 
-extern const double PI;
-extern const double E;
 
 
 int main()
 {
-  printf("  PI: %f\n",PI);
-  printf("   E: %f\n",E);
+  
+  printf("  PI: %f\n", PI);
+  printf("   E: %f\n", E);
   return 0;
 }
+
+/*Po kompilacji i konsolidacji za pomocą poleceń:
+gcc -c -Wall -pedantic modul.c
+gcc -c -Wall -pedantic program.c
+gcc -Wall program.o modul.o
+program nie wyświetlił niczego na ekranie.
+
+Przy kompilacji i konsolidacji za pomocą poleceń 
+g++ -c -Wall -pedantic modul.c
+g++ -c -Wall -pedantic program.c
+g++ -Wall program.o modul.o 
+
+program wyświetlił komunikat o nieznanej referencji do stalych PI oraz E*/
